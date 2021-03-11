cf4ac54668785195c982b4cf1bea848574e3ded7
Alexia <alexiagconti@gmail.com>
2021-03-11 11:39:48 -0300

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React  from "react";
+
+export const NavBar = () => {
+    return (
+        <div>
+            <h3>Alexia - Galería de Arte</h3>
+            <ul>
+                <li><a href="">Obras</a></li>
+                <li><a href="">Obras2</a></li>
+                <li><a href="">Obras3</a></li>
+            </ul>
+        </div> 
+    ); 
+};
